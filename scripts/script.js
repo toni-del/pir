4ea38f243ab8c86b9583cb6d5172ef822c500684
toni-del <60567665+toni-del@users.noreply.github.com>
2022-02-06 22:16:04 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,17 @@ function antiblacker(x) {
 	e = document.getElementById("gr"+x)
 	e.className ="i"
 }
+b = true
+function start(x, y) {
+	e = document.getElementById(x)
+	a = document.getElementById(y)
+	if (b) {
+		b = false
+	e.play()
+	a.className = "non"
+	} else {
+	a.className = ""
+	e.pause()
+	b = true
+}
+}
